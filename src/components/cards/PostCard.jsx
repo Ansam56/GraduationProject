74e83f47963cfd499afb0cc5ed7b417ad98ec930
Ansam Janajreh <86476980+Ansam56@users.noreply.github.com>
2024-11-15 11:06:45 +0200

    Update PostCard.jsx

--- src/components/cards/PostCard.jsx
+++ src/components/cards/PostCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardHeader,
@@ -20,30 +20,14 @@ import { ar } from "date-fns/locale";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useNavigate } from "react-router-dom";
-import "./PostCard.css"; 
+import "./PostCard.css";
 
 const PostCard = ({ post = null, handleDeletePost }) => {
   const navigate = useNavigate();
   const [showMoreText, setShowMoreText] = useState(false);
   const [showDialog, setShowDialog] = useState(false);
 
-  const fakePost = {
-    id: "1",
-    content:
-      "القُرآن ملاذ وأمان المؤمن من تولَّى عنه فقد أقبل على حياة موحشة ومن أقبل عليه بصدق أقبلت عليه من الله الرحمات وغمرته ألطافه بالعون والسداد وبركة الأوقات يدعوكم ملتقى القرآن الكريم في جامعة فلسطين التقنية خضوري لحضور لقاء ملاذُ الحافظين مع الشيخ موسى مدفع وذلك اليوم الاثنين عند الساعة التاسعة والنصف مساءً عبر تقنية زوم",
-    title: "دعوة لحضور لقاء ملاذُ الحافظين",
-    createdAt: new Date(),
-    attachment: {
-      url: "https://scontent.fjrs27-1.fna.fbcdn.net/v/t39.30808-6/441639189_1036342611364277_2818803549453965447_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=XqDQ_mFstCYQ7kNvgGpkfcB&_nc_zt=23&_nc_ht=scontent.fjrs27-1.fna&_nc_gid=ARXlJFzMXXL82_h78p6XtCB&oh=00_AYBrV1Z9eoPkiLWU_wbhWpFCrp0xtw6GqZbqziAVyaX5dA&oe=67352F2D",
-    },
-    user: {
-      firstName: "شيماء",
-      lastName: "عماد",
-      profilePicture: "https://via.placeholder.com/50",
-    },
-  };
-
-  const activePost = post || fakePost;
+  const activePost = post;
 
   const handleDialogClose = () => setShowDialog(false);
   const handleDialogOpen = () => setShowDialog(true);
@@ -62,25 +46,39 @@ const PostCard = ({ post = null, handleDeletePost }) => {
     <Card className="post-card">
       <CardHeader
         avatar={
-          <Avatar
-            src={activePost.user.profilePicture}
-            className="profile-picture"
-          />
+          activePost.user ? (
+            <Avatar
+              src={activePost.user.profilePicture}
+              alt={`${activePost.user.firstName} ${activePost.user.lastName}`}
+              sx={{
+                bgcolor: activePost.user.profilePicture
+                  ? "transparent"
+                  : "#3f51b5",
+              }}
+            >
+              {!activePost.user.profilePicture &&
+                `${activePost.user.firstName[0]}${activePost.user.lastName[0]}`}
+            </Avatar>
+          ) : null
         }
         title={
-          <div className="author-section">
-            <div className="author-name-time">
-              <Typography className="author-name">
-                {`${activePost.user.firstName} ${activePost.user.lastName}`}
-              </Typography>
-              <Typography className="date-no-image">
-                {formatDistanceToNow(activePost.createdAt, {
-                  addSuffix: true,
-                  locale: ar,
-                })}
-              </Typography>
+          activePost.user ? (
+            <div className="author-section">
+              <div className="author-name-time">
+                <Typography className="author-name">
+                  {`${activePost.user.firstName} ${activePost.user.lastName}`}
+                </Typography>
+                <Typography className="date-no-image">
+                  {formatDistanceToNow(activePost.createdAt, {
+                    addSuffix: true,
+                    locale: ar,
+                  })}
+                </Typography>
+              </div>
             </div>
-          </div>
+          ) : (
+            "Unknown User"
+          )
         }
         className="card-header"
       />
@@ -92,7 +90,7 @@ const PostCard = ({ post = null, handleDeletePost }) => {
           sx={{
             width: "100%",
             height: "auto",
-            maxHeight: "400px",
+            maxHeight: "300px",
           }}
           image={activePost.attachment.url}
           alt="Post Attachment"
