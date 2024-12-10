ea37710b65af532e7583d0f8b819b0dce0f3c39b
Sahar Sous <81341976+Sahar-sous72@users.noreply.github.com>
2024-12-10 22:27:36 +0200

    schools

---
+++ BackEnd/DB/models/schools/school.js
@@ -0,0 +1,40 @@
+import { model, Schema, Types } from "mongoose";
+import mongoose from 'mongoose';
+
+const schoolSchema =new Schema({
+    schoolName:{
+        type:String,
+        required:true,
+        unique:true
+    },address:{
+        type:String,
+        required:true
+    },
+   schoolInfo:{
+        type:Object
+    },
+    schoolPhoto:{
+     type:Object
+    },
+    schoolAdminId:{
+        type:Types.ObjectId,
+        ref:'schoolAdmin',
+        required:true
+    
+       },
+       description:{
+        type:String
+       },
+    status:{
+        type:String,
+        default:'suspend',
+        enum:['active','suspend','rejected']
+    }
+        
+},{
+    toJSON:{virtuals:true},  // عشان الفيرتشول
+    toObject:{virtuals:true},
+    timestamps:true
+})
+const schoolModel =mongoose.model('School',schoolSchema)
+export default schoolModel;
