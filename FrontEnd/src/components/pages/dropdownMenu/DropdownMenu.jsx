c8d21fc72579c3870f97ca1358ed0bac40a6fefe
Raghad-Moqady <raghadmoqady@gmail.com>
2025-01-13 14:48:07 +0300

    edit dropdownMenu

--- FrontEnd/src/components/pages/dropdownMenu/DropdownMenu.jsx
+++ FrontEnd/src/components/pages/dropdownMenu/DropdownMenu.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../../context/UserContext";
+import { Skeleton } from "@mui/material";
 
 export default function DropdownMenu() {
   let {userData,Logout}=useContext(UserContext);
   return (
     <ul className="navbar-nav ms-2">
       <li className="nav-item dropdown">
-        <a
+        {userData?  <a
           className="nav-link dropdown-toggle"
           href="#"
           role="button"
@@ -15,7 +16,8 @@ export default function DropdownMenu() {
           aria-expanded="false"
         >
            {userData?.userName}
-        </a>
+        </a>:<Skeleton variant="text" sx={{ fontSize: '1rem',width:"8rem" }} />}
+      
         <ul className="dropdown-menu ">  
             <li>
               <Link className="dropdown-item text-end" to="/">
