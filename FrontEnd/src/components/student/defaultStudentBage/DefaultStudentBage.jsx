d130fe9932dbf211c93dc58baec1c614345efb02
Ansam Janajreh <86476980+Ansam56@users.noreply.github.com>
2025-01-30 14:18:54 +0200

    edit DefaultStudentBage

--- FrontEnd/src/components/student/defaultStudentBage/DefaultStudentBage.jsx
+++ FrontEnd/src/components/student/defaultStudentBage/DefaultStudentBage.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import WavingHandOutlinedIcon from '@mui/icons-material/WavingHandOutlined';
+import React from "react";
+import WavingHandOutlinedIcon from "@mui/icons-material/WavingHandOutlined";
+import ProfileCard from "./ProfileCard";
 
 export default function DefaultStudentBage() {
   return (
     <>
-        <h2>أهلا وسهلا بك ,رغد موقدي  <WavingHandOutlinedIcon/></h2>  
+      <h2>
+        أهلا بك في حلقتك القرآنية
+        <WavingHandOutlinedIcon />
+      </h2>
+      <ProfileCard />
     </>
-  )
+  );
 }
