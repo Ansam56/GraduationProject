f6d8d10297e82652fa88254e2a8b73d386ab17b6
Ansam Janajreh <86476980+Ansam56@users.noreply.github.com>
2025-01-30 14:21:01 +0200

    edit DefaultTeacherBage.jsx

--- FrontEnd/src/components/teacher/defaultTeacherBage/DefaultTeacherBage.jsx
+++ FrontEnd/src/components/teacher/defaultTeacherBage/DefaultTeacherBage.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import WavingHandOutlinedIcon from '@mui/icons-material/WavingHandOutlined';
+import React from "react";
+import WavingHandOutlinedIcon from "@mui/icons-material/WavingHandOutlined";
+import ProfileCard from "./ProfileCard";
 export default function DefaultTeacherBage() {
   return (
-    <> 
-    <h2>أهلا وسهلا بك ,أ.رغد موقدي  <WavingHandOutlinedIcon/></h2>  
+    <>
+      <h2>
+        أهلا بك في حلقتك القرآنية
+        <WavingHandOutlinedIcon />
+      </h2>
+      <ProfileCard />
     </>
-    
-  )
+  );
 }
