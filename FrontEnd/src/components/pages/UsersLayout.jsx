443715f5a0f29f87e5cd1b22032d8a64c8b627cf
Ansam Janajreh <86476980+Ansam56@users.noreply.github.com>
2025-01-31 20:15:34 +0200

    edit UsersLayout

--- FrontEnd/src/components/pages/UsersLayout.jsx
+++ FrontEnd/src/components/pages/UsersLayout.jsx
@@ -2,24 +2,24 @@ import React, { useContext, useState } from "react";
 import { Outlet } from "react-router-dom";
 import PersistentDrawerRight from "./sharedDashboard/PersistentDrawerRight";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
- 
+
 import Logo from "./logo/Logo";
 import { SchoolAdminContext } from "../context/SchoolAdminContext";
 import { TeacherContext } from "../context/TeacherContext";
 import { UserContext } from "../context/UserContext";
-import { StudentContext } from "../context/StudentContext"; 
-import DescriptionIcon from '@mui/icons-material/Description';
-import  HomeIcon  from '@mui/icons-material/Home';
-import SettingsIcon from '@mui/icons-material/Settings';
-import GroupAddIcon from '@mui/icons-material/GroupAdd';
-import SchoolIcon from '@mui/icons-material/School';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import PieChartIcon from '@mui/icons-material/PieChart';
-import NewspaperIcon from '@mui/icons-material/Newspaper';
+import { StudentContext } from "../context/StudentContext";
+import DescriptionIcon from "@mui/icons-material/Description";
+import HomeIcon from "@mui/icons-material/Home";
+import SettingsIcon from "@mui/icons-material/Settings";
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
+import SchoolIcon from "@mui/icons-material/School";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import PieChartIcon from "@mui/icons-material/PieChart";
+import NewspaperIcon from "@mui/icons-material/Newspaper";
 
 export default function UsersLayout() {
-  let {userData}=useContext(UserContext);
+  let { userData } = useContext(UserContext);
 
   let SideBarLinks = [];
   let NavTitle = "";
@@ -27,7 +27,7 @@ export default function UsersLayout() {
   let image;
 
   if (userData?.role === "schoolAdmin") {
-    let {schoolAdminInfo,schoolInfo}=useContext(SchoolAdminContext); 
+    let { schoolAdminInfo, schoolInfo } = useContext(SchoolAdminContext);
     SideBarLinks = [
       {
         name: "عرض طلبات انضمام المعلمين",
@@ -41,26 +41,25 @@ export default function UsersLayout() {
       },
       {
         name: "الأخبار",
-        icon: <NewspaperIcon/>,
+        icon: <NewspaperIcon />,
         target: "/SchoolAdmin/posts",
       },
       {
-        name:"تقرير انجاز الحلقات",
+        name: "تقرير انجاز الحلقات",
         icon: <DescriptionIcon />,
         target: "/SchoolAdmin/CirclesAchievementsReport",
-      }
+      },
       //  {
       //   name:"احصائيات المدرسة",
       //   icon:<InboxIcon />,
       //   target:"/SchoolAdmin/ProfileSettings"
       //   },
     ];
-    NavTitle =schoolInfo?.schoolName;
+    NavTitle = schoolInfo?.schoolName;
     SideBarTitle = "بوابة الإدارة";
-    image=schoolAdminInfo?.profilePicture.secure_url;
-
+    image = schoolAdminInfo?.profilePicture.secure_url;
   } else if (userData?.role === "teacher") {
-    let {circleInfo,teacherInfo,schoolName}=useContext(TeacherContext);
+    let { circleInfo, teacherInfo, schoolName } = useContext(TeacherContext);
     SideBarLinks = [
       {
         name: "الرئيسية",
@@ -69,7 +68,7 @@ export default function UsersLayout() {
       },
       {
         name: "اعدادات الحساب",
-        icon: <SettingsIcon/>,
+        icon: <SettingsIcon />,
         target: "/Teacher/ProfileSettings",
       },
       {
@@ -83,22 +82,26 @@ export default function UsersLayout() {
         target: "/Teacher/StudentManagement",
       },
       {
-        name: "التقارير", 
-        icon: <BarChartIcon />, 
+        name: "التقارير",
+        icon: <BarChartIcon />,
         children: [
           {
             name: "تقرير الانجاز اليومي",
             icon: <DescriptionIcon />,
             target: "/Teacher/DailyAchievementReport",
-          } ,
-          
+          },
         ],
       },
       {
         name: "الاحصاءات",
         // subName: "احصاءات الانجاز ",
-        icon: <PieChartIcon/>,
+        icon: <PieChartIcon />,
         // target: "/Teacher/DailyAchievementReport",
+      },
+      {
+        name: "الأخبار",
+        icon: <NewspaperIcon />,
+        target: "/Teacher/posts",
       },
       {
         name: "الاختبارات",
@@ -107,9 +110,9 @@ export default function UsersLayout() {
       },
     ];
     // NavTitle = "ملتقى فلسطين التقنية خضوري > بالقرآن نحيا";
-    NavTitle =`${schoolName} > ${circleInfo?.circleName}` ;
+    NavTitle = `${schoolName} > ${circleInfo?.circleName}`;
     SideBarTitle = "بوابة المعلم";
-    image=teacherInfo?.profilePicture.secure_url;
+    image = teacherInfo?.profilePicture.secure_url;
   } else if (userData?.role === "admin") {
     SideBarLinks = [
       {
@@ -120,11 +123,11 @@ export default function UsersLayout() {
     ];
     NavTitle = "موقع طوبى";
     SideBarTitle = " بوابة المسؤولون";
-  } else if (userData?.role === "student"||userData?.role==="user") {
-    let {studentInfo,circleName,schoolName}=useContext(StudentContext); 
+  } else if (userData?.role === "student" || userData?.role === "user") {
+    let { studentInfo, circleName, schoolName } = useContext(StudentContext);
     console.log(studentInfo);
     console.log(userData);
-    if(userData?.role==="student" &&userData?.status==="active"){
+    if (userData?.role === "student" && userData?.status === "active") {
       SideBarLinks = [
         {
           name: "الرئيسية",
@@ -141,33 +144,40 @@ export default function UsersLayout() {
           icon: <DescriptionIcon />,
           target: "/Student/DailyAchievementReport",
         },
-         {
+        {
+          name: "الأخبار",
+          icon: <NewspaperIcon />,
+          target: "/student/posts",
+        },
+        {
           name: "الاختبارات",
           icon: <AssignmentIcon />,
           target: "/Student/Exams",
-        }
+        },
       ];
       NavTitle = `${schoolName} > ${circleName}`;
-    }else if(userData?.role==="user"||(userData?.role==="student"&&userData?.status==="suspend")){
+    } else if (
+      userData?.role === "user" ||
+      (userData?.role === "student" && userData?.status === "suspend")
+    ) {
       SideBarLinks = [
         {
           name: "الرئيسية",
           icon: <HomeIcon />,
           target: "/Student",
-        } ,
+        },
         {
           //هاي لازم انقلها للطالب الزائر بس مؤقتا رح أشتغلها هون
-          name:"حلقات المدرسة",
-          icon :<SchoolIcon />,
-          target:`/Student/SchoolCircles/${studentInfo?.schoolId}`
+          name: "حلقات المدرسة",
+          icon: <SchoolIcon />,
+          target: `/Student/SchoolCircles/${studentInfo?.schoolId}`,
         },
-         
       ];
       NavTitle = `مرحباً بك في المدرسة القرآنية : ${schoolName} `;
     }
     //from studentInfo
     SideBarTitle = "بوابة الطالب";
-    image=studentInfo?.profilePicture.secure_url;
+    image = studentInfo?.profilePicture.secure_url;
   }
 
   return (
