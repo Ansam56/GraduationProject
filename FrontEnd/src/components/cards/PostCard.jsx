b273a97f9cd29d1fd1274aebcb7883dc36d9a779
Ansam Janajreh <86476980+Ansam56@users.noreply.github.com>
2025-01-31 20:10:13 +0200

    edit PostCard

--- FrontEnd/src/components/cards/PostCard.jsx
+++ FrontEnd/src/components/cards/PostCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import {
   Card,
   CardHeader,
@@ -12,89 +12,107 @@ import {
   Dialog,
   DialogActions,
   DialogContent,
-  DialogContentText,
   DialogTitle,
 } from "@mui/material";
-import { formatDistanceToNow } from "date-fns";
+import { formatDistanceToNow, parseISO } from "date-fns";
 import { ar } from "date-fns/locale";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { UserContext } from "../../components/context/UserContext"; 
 import "./PostCard.css";
 
 const PostCard = ({ post = null, handleDeletePost }) => {
   const navigate = useNavigate();
-  const [showMoreText, setShowMoreText] = useState(false);
   const [showDialog, setShowDialog] = useState(false);
+  const [postDialog, setPostDialog] = useState(false);
+  const { userData } = useContext(UserContext); 
 
   const activePost = post;
 
   const handleDialogClose = () => setShowDialog(false);
   const handleDialogOpen = () => setShowDialog(true);
-  const handleDialogConfirm = () => {
-    handleDeletePost(activePost.id);
-    setShowDialog(false);
-    toast.success("تم حذف الخبر");
+  const handleDialogConfirm = async () => {
+    try {
+      await handleDeletePost(activePost._id); 
+      setShowDialog(false);
+    } catch (error) {
+      console.error("Error deleting post:", error);
+      toast.error("فشل في حذف الخبر");
+    }
   };
 
+  const handlePostDialogOpen = () => setPostDialog(true);
+  const handlePostDialogClose = () => setPostDialog(false);
+
   const truncatedText =
-    activePost.content.length > 120
-      ? `${activePost.content.substring(0, 120)}...`
-      : activePost.content;
-  const handleTextToggle = () => setShowMoreText(!showMoreText);
+    activePost.subject.length > 250
+      ? `${activePost.subject.substring(0, 250)}...`
+      : activePost.subject;
 
   return (
     <Card className="post-card">
       <CardHeader
         avatar={
-          activePost.user ? (
+          activePost.schoolAdminPhoto?.secure_url ? (
             <Avatar
-              src={activePost.user.profilePicture}
-              alt={`${activePost.user.firstName} ${activePost.user.lastName}`}
+              src={activePost?.schoolAdminPhoto?.secure_url}
               sx={{
-                bgcolor: activePost.user.profilePicture
-                  ? "transparent"
-                  : "#3f51b5",
+                width: 50,
+                height: 50,
+                margin: "8px auto",
+              }}
+            />
+          ) : (
+            <Avatar
+              sx={{
+                bgcolor: "#3f51b5",
+                color: "white",
+                fontSize: "1rem",
+                margin: "8px auto",
               }}
             >
-              {!activePost.user.profilePicture &&
-                `${activePost.user.firstName[0]}${activePost.user.lastName[0]}`}
+              {activePost.schoolAdminName
+                ? activePost.schoolAdminName[0] + activePost.schoolAdminName[1]
+                : "A"}
             </Avatar>
-          ) : null
+          )
         }
         title={
-          activePost.user ? (
-            <div className="author-section">
-              <div className="author-name-time">
-                <Typography className="author-name">
-                  {`${activePost.user.firstName} ${activePost.user.lastName}`}
-                </Typography>
-                <Typography className="date-no-image">
-                  {formatDistanceToNow(activePost.createdAt, {
-                    addSuffix: true,
-                    locale: ar,
-                  })}
-                </Typography>
-              </div>
-            </div>
-          ) : (
-            "Unknown User"
-          )
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              marginRight: "5px",
+            }}
+          >
+            <Typography className="author-name">
+              {activePost.schoolAdminName || "Unknown User"}
+            </Typography>
+            <Typography
+              className="date-no-image"
+              sx={{ fontSize: "0.85rem", color: "gray" }}
+            >
+              {formatDistanceToNow(
+                parseISO(`${activePost.newsDate}T${activePost.newsTime}Z`),
+                {
+                  addSuffix: true,
+                  locale: ar,
+                }
+              )}
+            </Typography>
+          </div>
         }
         className="card-header"
       />
 
-      {activePost.attachment && activePost.attachment.url && (
+      {activePost.photo && activePost.photo?.secure_url && (
         <CardMedia
           component="img"
           className="post-section"
-          sx={{
-            width: "100%",
-            height: "auto",
-            maxHeight: "300px",
-          }}
-          image={activePost.attachment.url}
+          sx={{ width: "100%", height: "auto", maxHeight: "300px" }}
+          image={activePost.photo?.secure_url}
           alt="Post Attachment"
         />
       )}
@@ -108,41 +126,110 @@ const PostCard = ({ post = null, handleDeletePost }) => {
           color="textSecondary"
           className="card-text"
           dangerouslySetInnerHTML={{
-            __html: showMoreText ? activePost.content : truncatedText,
+            __html: truncatedText,
           }}
         />
       </CardContent>
 
-      {truncatedText !== activePost.content && (
+      {truncatedText !== activePost.subject && (
         <CardActions className="post-card-footer">
-          <Button onClick={handleTextToggle} className="read-more-btn">
-            {showMoreText ? "اخفاء النص" : "قراءة المزيد"}
+          <Button onClick={handlePostDialogOpen} className="read-more-btn">
+            قراءة المزيد
           </Button>
         </CardActions>
       )}
 
-      <CardActions disableSpacing className="post-edit-delete">
-        <IconButton onClick={() => navigate("../PostForm/:id")}>
-          <EditIcon color="primary" />
-        </IconButton>
-        <IconButton onClick={handleDialogOpen}>
-          <DeleteIcon color="error" />
-        </IconButton>
-      </CardActions>
+      {userData?.role === "schoolAdmin" && (
+        <CardActions disableSpacing className="post-edit-delete">
+          <IconButton
+            onClick={() =>
+              navigate(`/SchoolAdmin/posts/edit/${activePost._id}`, {
+                state: {
+                  post: {
+                    ...activePost,
+                    subject: activePost.subject || "<p></p>",
+                  },
+                },
+              })
+            }
+          >
+            <EditIcon color="primary" />
+          </IconButton>
+          <IconButton onClick={handleDialogOpen}>
+            <DeleteIcon color="error" />
+          </IconButton>
+        </CardActions>
+      )}
 
       <Dialog open={showDialog} onClose={handleDialogClose}>
-        <DialogTitle>تأكيد الحذف</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            هل أنت متأكد أنك تريد حذف هذا المنشور؟
-          </DialogContentText>
-        </DialogContent>
+        <DialogTitle> هل أنت متأكد أنك تريد حذف هذا الخبر ؟</DialogTitle>
         <DialogActions>
-          <Button onClick={handleDialogClose} color="primary">
+          <Button
+            onClick={handleDialogClose}
+            color="primary"
+            sx={{
+              fontWeight: "bold",
+            }}
+          >
             الغاء
           </Button>
-          <Button onClick={handleDialogConfirm} color="error">
+          <Button
+            onClick={handleDialogConfirm}
+            color="error"
+            sx={{
+              fontWeight: "bold",
+            }}
+          >
             حذف
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog
+        open={postDialog}
+        onClose={handlePostDialogClose}
+        fullWidth
+        maxWidth="md"
+      >
+        <DialogTitle
+          sx={{
+            backgroundColor: "var(--highlight-color)",
+            color: "white",
+            fontWeight: "bold",
+          }}
+        >
+          {activePost.title}
+        </DialogTitle>
+        <DialogContent>
+          {activePost.photo && activePost.photo?.secure_url && (
+            <CardMedia
+              component="img"
+              sx={{
+                width: "100%",
+                maxHeight: "400px",
+                maxWidth: "400px",
+                margin: "20px auto", 
+                display: "block", 
+                // marginBottom: "10px",
+              }}
+              image={activePost.photo?.secure_url}
+              alt="Post Attachment"
+            />
+          )}
+          <Typography
+            variant="body1"
+            dangerouslySetInnerHTML={{ __html: activePost.subject }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handlePostDialogClose}
+            color="primary"
+            sx={{
+              fontWeight: "bold",
+            }}
+          >
+            اغلاق
           </Button>
         </DialogActions>
       </Dialog>
