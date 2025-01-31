bb307c516ac1307eb01790b8bc91a3feb19dd36a
Ansam Janajreh <86476980+Ansam56@users.noreply.github.com>
2025-01-31 20:04:33 +0200

    added posts routes

--- FrontEnd/src/routes.jsx
+++ FrontEnd/src/routes.jsx
@@ -4,7 +4,7 @@ import Login from "./components/authentication/login/Login";
 import SendCode from "./components/authentication/sendCode/SendCode";
 import ForgetPassword from "./components/authentication/forgetPassword/ForgetPassword";
 // import ScrollDialog from "./components/web/home/scrollingDialog/ScrollDialog";
-import Profile from "./components/schoolAdmin/profile/Profile"; 
+import Profile from "./components/schoolAdmin/profile/Profile";
 import UsersLayout from "./components/pages/UsersLayout";
 import TeacherProfile from "./components/teacher/profile/TeacherProfile";
 import StudentForm from "./components/forms/StudentForm";
@@ -16,7 +16,7 @@ import StudentsRequests from "./components/teacher/StudentsRequests/StudentsRequ
 // import EditProfile from "./components/schoolAdmin/editProfile/EditProfile";
 import FeaturesSP from "./components/web/navbarSections/FeaturesSP";
 import SchoolsSP from "./components/web/navbarSections/SchoolsSP";
-import StatisticsSP from "./components/web/navbarSections/StatisticsSP"; 
+import StatisticsSP from "./components/web/navbarSections/StatisticsSP";
 import PostForm from "./components/forms/PostForm";
 import Posts from "./components/pages/Posts/Posts";
 import StudentManagement from "./components/teacher/studentManagement/StudentManagement";
@@ -24,243 +24,288 @@ import DefaultTeacherBage from "./components/teacher/defaultTeacherBage/DefaultT
 import TeacherDailyAchaievements from "./components/teacher/reports/dailyAchaievements/TeacherDailyAchaievements";
 import DefaultStudentBage from "./components/student/defaultStudentBage/DefaultStudentBage";
 import StudentDailyAchaievements from "./components/student/reports/dailyAchaievements/StudentDailyAchaievements";
-import ExamTable from "./components/teacher/exam/ExamTable"; 
+import ExamTable from "./components/teacher/exam/ExamTable";
 import CirclesAchaievements from "./components/schoolAdmin/reports/circlesAchaievements/CirclesAchaievements";
 import ExamForm from "./components/forms/ExamForm";
 import Exams from "./components/student/Exams/Exams";
-import {createBrowserRouter } from "react-router-dom";  
+import { createBrowserRouter } from "react-router-dom";
 import UserProtectedRoute from "./components/protectedRoute/UserProtectedRoute";
- 
+
 import PageNotFound from "./components/pages/PageNotFound";
- 
+
 import AuthProtectedRoute from "./components/protectedRoute/AuthProtectedRoute";
 import SchoolAdminContextProvider from "./components/context/SchoolAdminContext";
 import TeacherContextProvider from "./components/context/TeacherContext";
 import StudentContextProvider from "./components/context/StudentContext";
 import StudentProfile from "./components/student/profile/StudentProfile";
 import SchoolCircles from "./components/student/schoolCircles/SchoolCircles";
- 
+import Certificates from "./components/teacher/Certificates/Certificates";
+import CertificateForm from "./components/forms/CertificateForm";
 
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <WebLayout/>,
-      children: [
-        {
-          // path :'/',
-          index: true,
-          element: <Home />,
-        },
-        {
-          path: "SchoolForm",
-          element: 
+  {
+    path: "/",
+    element: <WebLayout />,
+    children: [
+      {
+        // path :'/',
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "SchoolForm",
+        element: (
           <AuthProtectedRoute>
             <SchoolForm />
           </AuthProtectedRoute>
-        },
-        {
-          path: "StudentForm/:schoolId",
-          element: 
+        ),
+      },
+      {
+        path: "StudentForm/:schoolId",
+        element: (
           <AuthProtectedRoute>
             <StudentForm />
           </AuthProtectedRoute>
-        },
-        {
-          path: "TeacherForm/:schoolId",
-          element:
+        ),
+      },
+      {
+        path: "TeacherForm/:schoolId",
+        element: (
           <AuthProtectedRoute>
             <TeacherForm />
           </AuthProtectedRoute>
-        },
-        {
-          path: "*",
-         element:<PageNotFound/>,
-        },
-        {
-          path: "login",
-          element:
+        ),
+      },
+      {
+        path: "*",
+        element: <PageNotFound />,
+      },
+      {
+        path: "login",
+        element: (
           <AuthProtectedRoute>
-            <Login/>
+            <Login />
           </AuthProtectedRoute>
-           
-        },
+        ),
+      },
 
-        {
-          path: "sendCode",
-          element: 
-          <AuthProtectedRoute> 
+      {
+        path: "sendCode",
+        element: (
+          <AuthProtectedRoute>
             <SendCode />
-          </AuthProtectedRoute> 
-        },
-        {
-          path: "forgetPassword",
-          element:
-          <AuthProtectedRoute> 
+          </AuthProtectedRoute>
+        ),
+      },
+      {
+        path: "forgetPassword",
+        element: (
+          <AuthProtectedRoute>
             <ForgetPassword />
           </AuthProtectedRoute>
-        },
-        {
-          path: "features",
-          element: <FeaturesSP />,
-        },
-        {
-          path: "schools",
-          element: <SchoolsSP />,
-        },
-        {
-          path: "statistics",
-          element: <StatisticsSP />,
-        },
-      ],
-    },
-    {
-      path: "/Admin",
-      element:
-      <UserProtectedRoute role="admin" >
-        <UsersLayout /> 
-      </UserProtectedRoute>,
-      //الشيلدرن بوخدها من الكومبوننت الخاصة بكل يوزر
-      children: [
-        {
-          index: true,
-          element: <Admin />,
-        },
-        {
-          path: "*",
-          element: <p className="m-0">Not Found Page</p>,
-        },
-        {
-          path: "SchoolRequests",
-          element: <Admin />,
-        },
-      ],
-    },
-    {
-      path: "/SchoolAdmin",
-      element:
-      <UserProtectedRoute role="schoolAdmin">
-      <SchoolAdminContextProvider> {/* ما رح يوصل هون الا والرول اله سكول ادمن */}
-            <UsersLayout  />
-      </SchoolAdminContextProvider>
-         </UserProtectedRoute>,
-      //الشيلدرن بوخدها من الكومبوننت الخاصة بكل يوزر
-      children: [
-        // {
-        //   // path :'/',
-        //   index: true,
-        //   element: <Home />,
-        // },
-        {
-          path: "*",
-          element: <p className="m-0">Not Found Page</p>,
-        },
-        {
-          
-          path: "ProfileSettings",
-          element: <Profile />,
-        },
-        // {
-        //   path: "EditProfile",
-        //   element: <EditProfile />,
-        // },
-        {
-          path: "TeachersRequests",
-          element: <TeachersRequests />,
-        },
-        {
-          path: "PostForm",
-          element: <PostForm />,
-        },
-        {
-          path: "Posts",
-          element: <Posts />,
-        },
-        {
-          path:"CirclesAchievementsReport",
-          element:<CirclesAchaievements/>,
-        }
-      ],
-    },
-    {
-      path: "/Teacher",
-      element:
-      <UserProtectedRoute role="teacher" >  {/* تعتمد على الuserContext */}
-      <TeacherContextProvider>
+        ),
+      },
+      {
+        path: "features",
+        element: <FeaturesSP />,
+      },
+      {
+        path: "schools",
+        element: <SchoolsSP />,
+      },
+      {
+        path: "statistics",
+        element: <StatisticsSP />,
+      },
+    ],
+  },
+  {
+    path: "/Admin",
+    element: (
+      <UserProtectedRoute role="admin">
         <UsersLayout />
-      </TeacherContextProvider>
-      </UserProtectedRoute> ,
-      //الشيلدرن بوخدها من الكومبوننت الخاصة بكل يوزر
-      children: [
-        {
-          // path :'/',
-          index: true,
-          element: <DefaultTeacherBage/>,
-        },
-        {
-          path: "*",
-          element: <p className="m-0">Not Found Page</p>,
-        },
-        {
-          path: "ProfileSettings",
-          element: <TeacherProfile />,
-        },
-        {
-          path: "StudentsRequests",
-          element: <StudentsRequests />,
-        },
-        {
-          path:"StudentManagement",
-          element:<StudentManagement/>
-        },
-        {
-          path:"DailyAchievementReport",
-          element:<TeacherDailyAchaievements/>
-        },
-       {
-          path: "exams",
-          element: <ExamTable />,
-        },
-        {
-          path: "ExamForm",
-          element: <ExamForm />,
-        },
-      ],
-    },
-    {
-      path: "/Student",
-      element: 
-      <UserProtectedRoute role="student or user" role1="student" role2="user">  {/* بالحالتين لازم الطالب يقدر يوصل للداشبورد اللي اله  */}
-        <StudentContextProvider> 
-        <UsersLayout />  {/*  (هل هو طالب زائر او طالب رسمي)سيتم هنا تحديد شكل الداشبورد للطالب */}
+      </UserProtectedRoute>
+    ),
+    //الشيلدرن بوخدها من الكومبوننت الخاصة بكل يوزر
+    children: [
+      {
+        index: true,
+        element: <Admin />,
+      },
+      {
+        path: "*",
+        element: <p className="m-0">Not Found Page</p>,
+      },
+      {
+        path: "SchoolRequests",
+        element: <Admin />,
+      },
+    ],
+  },
+  {
+    path: "/SchoolAdmin",
+    element: (
+      <UserProtectedRoute role="schoolAdmin">
+        <SchoolAdminContextProvider>
+          {" "}
+          {/* ما رح يوصل هون الا والرول اله سكول ادمن */}
+          <UsersLayout />
+        </SchoolAdminContextProvider>
+      </UserProtectedRoute>
+    ),
+    //الشيلدرن بوخدها من الكومبوننت الخاصة بكل يوزر
+    children: [
+      // {
+      //   // path :'/',
+      //   index: true,
+      //   element: <Home />,
+      // },
+      {
+        path: "*",
+        element: <p className="m-0">Not Found Page</p>,
+      },
+      {
+        path: "ProfileSettings",
+        element: <Profile />,
+      },
+      // {
+      //   path: "EditProfile",
+      //   element: <EditProfile />,
+      // },
+      {
+        path: "TeachersRequests",
+        element: <TeachersRequests />,
+      },
+      {
+        path: "PostForm",
+        element: <PostForm />,
+      },
+      {
+        path: "Posts",
+        children: [
+          {
+            path: "edit/:newsId", // Full path: /SchoolAdmin/posts/edit/:newsId
+            element: <PostForm />,
+          },
+          {
+            index: true,
+            element: <Posts />,
+          },
+        ],
+      },
+      {
+        path: "edit/:newsId", // Correct relative path
+        element: <PostForm />,
+      },
+      {
+        path: "CirclesAchievementsReport",
+        element: <CirclesAchaievements />,
+      },
+    ],
+  },
+  {
+    path: "/Teacher",
+    element: (
+      <UserProtectedRoute role="teacher">
+        {" "}
+        {/* تعتمد على الuserContext */}
+        <TeacherContextProvider>
+          <UsersLayout />
+        </TeacherContextProvider>
+      </UserProtectedRoute>
+    ),
+    //الشيلدرن بوخدها من الكومبوننت الخاصة بكل يوزر
+    children: [
+      {
+        // path :'/',
+        index: true,
+        element: <DefaultTeacherBage />,
+      },
+      {
+        path: "*",
+        element: <p className="m-0">Not Found Page</p>,
+      },
+      {
+        path: "ProfileSettings",
+        element: <TeacherProfile />,
+      },
+      {
+        path: "StudentsRequests",
+        element: <StudentsRequests />,
+      },
+      {
+        path: "StudentManagement",
+        element: <StudentManagement />,
+      },
+      {
+        path: "DailyAchievementReport",
+        element: <TeacherDailyAchaievements />,
+      },
+      {
+        path: "Posts",
+        element: <Posts />,
+      },
+      {
+        path: "exams",
+        element: <ExamTable />,
+      },
+      {
+        path: "ExamForm",
+        element: <ExamForm />,
+      },
+      {
+        path: "Certificates",
+        element: <Certificates />,
+      },
+      {
+        path: "CertificateForm",
+        element: <CertificateForm />,
+      },
+    ],
+  },
+  {
+    path: "/Student",
+    element: (
+      <UserProtectedRoute role="student or user" role1="student" role2="user">
+        {" "}
+        {/* بالحالتين لازم الطالب يقدر يوصل للداشبورد اللي اله  */}
+        <StudentContextProvider>
+          <UsersLayout />{" "}
+          {/*  (هل هو طالب زائر او طالب رسمي)سيتم هنا تحديد شكل الداشبورد للطالب */}
         </StudentContextProvider>
-      </UserProtectedRoute>,
-      //الشيلدرن بوخدها من الكومبوننت الخاصة بكل يوزر
-      children: [
-        { // path :'/',
-          index: true,
-          element: <DefaultStudentBage/>,
-        },
-        {
-          path: "Profile",
-          element: <StudentProfile/>,
-        },
-        {
-          path: "*",
-          element: <p className="m-0">Not Found Page</p>,
-        } ,
-        {
-          path:"DailyAchievementReport",
-          element:<StudentDailyAchaievements/>
-        },
-       {
-          path: "Exams",
-          element: <Exams />,
-        },
-        {
-          path:"SchoolCircles/:schoolId",
-          element:<SchoolCircles/>
-        }
-       
-      ],
-    },
-  ]);
+      </UserProtectedRoute>
+    ),
+    //الشيلدرن بوخدها من الكومبوننت الخاصة بكل يوزر
+    children: [
+      {
+        // path :'/',
+        index: true,
+        element: <DefaultStudentBage />,
+      },
+      {
+        path: "Profile",
+        element: <StudentProfile />,
+      },
+      {
+        path: "*",
+        element: <p className="m-0">Not Found Page</p>,
+      },
+      {
+        path: "DailyAchievementReport",
+        element: <StudentDailyAchaievements />,
+      },
+      {
+        path: "Posts",
+        element: <Posts />,
+      },
+      {
+        path: "Exams",
+        element: <Exams />,
+      },
+      {
+        path: "SchoolCircles/:schoolId",
+        element: <SchoolCircles />,
+      },
+    ],
+  },
+]);
